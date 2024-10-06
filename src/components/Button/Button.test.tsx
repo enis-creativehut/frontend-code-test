85dd5e0e20483a76528cc850b9e99b4fdd8b5431
enis-creativehut <enis@creativehut.co.uk>
2024-10-06 18:36:00 +0100

    Improved test readability...

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -3,95 +3,90 @@ import { ButtonProps } from './types'
 import Button from './Button'
 
 describe('Button', () => {
-  describe('When renders and the button is disabled', () => {
-    const mockedProps: ButtonProps = {
-      title: 'test button',
-      startIcon: true,
-      disabled: true,
-      callbackFn: jest.fn(),
-      variant: '',
-    }
+  describe('When renders', () => {
+    describe('and the button is disabled', () => {
+      const mockedProps: ButtonProps = {
+        title: 'test button',
+        startIcon: true,
+        disabled: true,
+        callbackFn: jest.fn(),
+        variant: '',
+      }
 
-    beforeEach(() => {
-      jest.resetAllMocks()
+      beforeEach(() => {
+        jest.resetAllMocks()
 
-      render(<Button {...mockedProps} />)
+        render(<Button {...mockedProps} />)
+      })
+
+      it('Should display the button with the icon', () => {
+        expect(screen.getByRole('button')).toBeInTheDocument()
+        expect(screen.getByLabelText('check-icon')).toBeInTheDocument()
+      })
+
+      it('Should not invoke the callback function when the button is disabled', () => {
+        fireEvent.click(screen.getByText(/Button/i))
+
+        expect(mockedProps.callbackFn).not.toHaveBeenCalled()
+      })
     })
 
-    it('Should display the button', () => {
-      expect(screen.getByRole('button')).toBeInTheDocument()
+    describe('and the button is enabled', () => {
+      const mockedProps: ButtonProps = {
+        title: 'test button',
+        disabled: false,
+        callbackFn: jest.fn(),
+        variant: '',
+      }
+
+      beforeEach(() => {
+        jest.resetAllMocks()
+
+        render(<Button {...mockedProps} />)
+      })
+
+      it('Should display the button without the icon', () => {
+        expect(screen.getByRole('button')).toBeInTheDocument()
+        expect(screen.queryByLabelText('check-icon')).not.toBeInTheDocument()
+      })
+
+      it('Should invoke the callback function', () => {
+        fireEvent.click(screen.getByText(/button/i))
+
+        expect(mockedProps.callbackFn).toHaveBeenCalledTimes(1)
+      })
     })
 
-    it('Should display the button icon', () => {
-      expect(screen.getByLabelText('check-icon')).toBeInTheDocument()
-    })
+    describe('and the button is loading', () => {
+      const mockedProps: ButtonProps = {
+        title: 'test button',
+        callbackFn: jest.fn(),
+        variant: '',
+        loading: true,
+      }
 
-    it('Should not trigger the callback function', () => {
-      fireEvent.click(screen.getByText(/Button/i))
+      beforeEach(() => {
+        jest.resetAllMocks()
 
-      expect(mockedProps.callbackFn).not.toHaveBeenCalled()
-    })
-  })
+        render(<Button {...mockedProps} />)
+      })
 
-  describe('When renders and the button is enabled', () => {
-    const mockedProps: ButtonProps = {
-      title: 'test button',
-      disabled: false,
-      callbackFn: jest.fn(),
-      variant: '',
-    }
+      it('Should display the button with the loading spinner', () => {
+        expect(screen.getByRole('button')).toBeInTheDocument()
+        expect(
+          screen.getByLabelText('loading-spinner-icon')
+        ).toBeInTheDocument()
+      })
 
-    beforeEach(() => {
-      jest.resetAllMocks()
+      it('Should not display the checked icon when loading', () => {
+        expect(screen.queryByLabelText('check-icon')).not.toBeInTheDocument()
+      })
 
-      render(<Button {...mockedProps} />)
-    })
+      it('Should not invoke the callback function when the button is loading', () => {
+        fireEvent.click(screen.getByRole('button'))
 
-    it('Should display the button', () => {
-      expect(screen.getByRole('button')).toBeInTheDocument()
-    })
-
-    it('Should not display the button icon', () => {
-      expect(screen.queryByLabelText('check-icon')).not.toBeInTheDocument()
-    })
-
-    it('Should trigger the callback function', () => {
-      fireEvent.click(screen.getByText(/button/i))
-
-      expect(mockedProps.callbackFn).toHaveBeenCalledTimes(1)
-    })
-  })
-
-  describe('When the button is loading', () => {
-    const mockedProps: ButtonProps = {
-      title: 'test button',
-      callbackFn: jest.fn(),
-      variant: '',
-      loading: true,
-    }
-
-    beforeEach(() => {
-      jest.resetAllMocks()
-
-      render(<Button {...mockedProps} />)
-    })
-
-    it('Should display the button', () => {
-      expect(screen.getByRole('button')).toBeInTheDocument()
-    })
-
-    it('Should display the the loading spinner', () => {
-      expect(screen.getByLabelText('loading-spinner-icon')).toBeInTheDocument()
-    })
-
-    it('Should not display the checked icon', () => {
-      expect(screen.queryByLabelText('check-icon')).not.toBeInTheDocument()
-    })
-
-    it('Should not trigger the callback function', () => {
-      fireEvent.click(screen.getByRole('button'))
-
-      expect(mockedProps.callbackFn).not.toHaveBeenCalled()
+        expect(mockedProps.callbackFn).not.toHaveBeenCalled()
+      })
     })
   })
 })
